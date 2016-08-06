e678fe368e5aa4990741db625b53b8da27b83606
Gurigraphics <gurigraphics@gmail.com>
2016-08-06 17:13:01 -0300

    Add Latino American Rhythm Pack
    I arrange the times in this image: [http://i.imgur.com/IQURaxM.png]
    And I created the Latino American Rhythm Pack to simplify the
    composition.

---
+++ src/rhythm-packs/latino-american.js
@@ -0,0 +1,30 @@
+/**
+ * Band.js - Music Composer
+ * An interface for the Web Audio API that supports rhythms, multiple instruments, repeating sections, and complex
+ * time signatures.
+ *
+ * @author Cody Lundquist (http://github.com/meenie) - 2014
+ */
+
+/**
+ * Latino American Rhythm Pack - Image reference: http://i.imgur.com/IQURaxM.png
+ */
+
+module.exports = {
+    1: 0.0625,
+    2: 0.125,
+    3: 0.1875,
+    4: 0.25,
+    5: 0.3125,
+    6: 0.375,
+    7: 0.4375,
+    8: 0.5,
+    9: 0.5625,
+    10: 0.625,
+    11: 0.6875,
+    12: 0.75,
+    13: 0.8125,
+    14: 0.875,
+    15: 0.9375,
+    16: 1
+};
